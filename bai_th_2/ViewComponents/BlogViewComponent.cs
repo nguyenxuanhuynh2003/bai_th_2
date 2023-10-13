801f9684486d378b5ce764922b4985af7f2ebd6f
Admin <Admin@NGUYENXUANHUYNH>
2023-10-13 07:30:58 +0700

    Add project files.

---
+++ bai_th_2/ViewComponents/BlogViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using bai_th_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bai_th_2.ViewComponents
+{
+    public class BlogViewComponent : ViewComponent
+    {
+        private readonly HarmicContext _context;
+        public BlogViewComponent(HarmicContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.TbBlogs.Where(m => (bool)m.IsActive).ToList();
+            return await Task.FromResult<IViewComponentResult>(View(items));
+        }
+
+    }
+}
---
+++ bai_th_2/ViewComponents/MenuTopViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using bai_th_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bai_th_2.Components
+{
+    public class MenuTopViewComponent : ViewComponent
+    {
+        
+            private readonly HarmicContext _context;
+
+            public MenuTopViewComponent(HarmicContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<IViewComponentResult> InvokeAsync()
+            {
+                var items = _context.TbMenus.Where(m => (bool)m.IsActive).OrderBy(m => m.Position).ToList();
+                return await Task.FromResult<IViewComponentResult>(View(items));
+            }
+    }
+}
